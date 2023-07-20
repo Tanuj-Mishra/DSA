dd6e564bbf2858b611ea58c057b1496b6fe9b8bb
Tanuj-Mishra <tanujtmmishra@gmail.com>
2023-07-20 18:49:42 +0530

    .

--- working.cpp
+++ working.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 int main() {
 
+    int a,b;
+    char ch;
+    cin >> a >> ch >> b;
+
+    switch(ch) {
+        case '+':
+            cout << a+b << endl;
+            break;
+        case '-':
+            cout << a-b << endl;
+            break;
+        case '*':
+            cout << a*b << endl;
+            break;
+        case '/':
+            cout << a/b << endl;
+            break;
+        default:
+            cout << "Invalid operator used" << endl;
+    }
 
     return 0;
 }
