cdbda2a578c3b5c53bd0e3dd8292a7373023ca84
Tanuj-Mishra <tanujtmmishra@gmail.com>
2023-07-21 16:55:31 +0530

    .

---
+++ 2. Fundamentals of Programming-1/Assignment-2/4. Predict Output.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int test = 0;
+    cout << "First Character " << '1' << endl;
+    cout << "Second Character " << (test ? 3 : '1') << endl;
+    return 0;
+}
