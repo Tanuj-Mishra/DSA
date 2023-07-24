dfe1cf990ad8e1b0931b927e9c475e534960c810
Tanuj-Mishra <tanujtmmishra@gmail.com>
2023-07-25 04:35:02 +0530

    .:

--- working.cpp
+++ working.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main() {
@@ -7,9 +8,22 @@ int main() {
     cout << "Enter n: ";
     cin >> n;
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n-i; j++) {
-            cout << "*";
+    bool print = 1;
+    string space = " ";
+    
+    // for(int row=0; row<n; row++) {
+    //     row%2==1 ? print=1 : print=0;
+    //     for(int col=0; col<=row; col++, print=!print) {
+    //         cout << print << space;
+    //     }
+    //     cout << endl;
+    // }
+
+    string zero = "0";
+    string one = "1";
+    for(int row=0; row<n; row++) {
+        for(int col=0; col<=row; col++) {
+            (row+col)%2==0 ? cout << one << space : cout << zero << space;
         }
         cout << endl;
     }
