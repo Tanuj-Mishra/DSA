e6912f4a3b0b5a6ba23f09f0ff681e87f2b6fde3
Tanuj-Mishra <tanujtmmishra@gmail.com>
2023-07-24 14:58:20 +0530

    .

--- working.cpp
+++ working.cpp
@@ -3,29 +3,99 @@ using namespace std;
 
 int main() {
 
-    int n1, n2;
-    cout << "Enter two numebers: ";
-    cin >> n1 >> n2;
-    
-    // Handling 0^0
-    if(n1==0 && n2==0) {
-        cout << "Invalid Entry" << endl;
-        return 0;
-    }
-    float ans=1;
-    float temp=n1;
+    // 1. Infinite Loop
 
-    // Handling when n2 is negative
-    if(n2<0) {  
-        temp=1/(float)n1;
-        n2*=-1;
-    }
+    // 2.
+    // Done in class
 
-    for(int i=1; i<=n2; i++) {
-        ans*=temp;
+    // 3. 
+/* 
+    In For Loop
+    In For Loop
+    In For Loop
+*/
+
+    // 4.
+    /*
+    9   1
+    8   2
+    7   3
+    6   4
+    5   5
+    4   6
+    */
+
+    // 5.
+    // int n;
+    // cout << "Enter number: ";
+    // cin >> n;
+
+    // int ans=0;
+    // while(n!=0) {
+    //     int lastDigit = n%10;
+    //     if(lastDigit%2==0) ans+=lastDigit;
+    //     n/=10;
+    // }
+    // cout << ans << endl;
+
+    // 6.
+    // int n;
+    // cout << "Enter number: ";
+    // cin >> n;
+    // int copy=n;
+
+    // int reverse=0;
+    // while(copy!=0) {
+    //     reverse = reverse*10 + copy%10;
+    //     copy/=10;
+    // }
+    // cout << (reverse+n) << endl;
+
+
+    // 7.
+    // int n;
+    // cout << "Enter number: ";
+    // cin >> n;
+
+    // int ans=1;
+    // if(n == 0) {
+    //     cout << 1 << endl;
+    //     return 0;
+    // }
+
+    // for(int i = 1; i<=n; i++) {
+    //     ans*=i;
+    //     cout << i << "! = " <<ans << endl;
+    // }
+
+    // 8.
+    // int n;
+    // cout << "Enter number: ";
+    // cin >> n;
+
+    // int a=1, b=1, c;
+    // for(int i =1; i <=n; i++) {
+    //     cout << i << "-> " << a << endl;
+    //     c=a+b;
+    //     a=b;
+    //     b=c;
+    // }
+
+    // 9.
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+    int copy=n;
+
+    int ans=0;
+    while(copy!=0) {
+        int lastDigit=copy%10;
+        ans+= lastDigit*lastDigit*lastDigit;
+        copy/=10;
     }
-    
-    cout << ans << endl;
+    ans==n ? cout << "Yes\n" : cout << "No\n";
+
+
 
     return 0;
 }
