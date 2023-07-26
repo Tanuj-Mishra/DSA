88c7c7278fc6acf9d4ef63c5e493c3d344264ac4
Tanuj-Mishra <tanujtmmishra@gmail.com>
2023-07-26 23:50:03 +0530

    .

--- working.cpp
+++ working.cpp
@@ -12,27 +12,30 @@ int main() {
     string print = "*";
     string space = " ";
 
-    int nsp=n-1;
-    int nst=1;
-    for(int row=0; row<2*n-1; row++) {
+    int nsp=-1;
+    int nst1=n;
+    int nst2=n-1;
+
+    for(int row=0; row<n; row++) {
+        if (row==1) nst2=n-1;
+
+        // star
+        for(int stars=0; stars<nst1; stars++) {
+            cout << print;
+        }
         // spaces
         for(int spaces=0; spaces < nsp; spaces++) {
             cout << space;
         }
-        // star
-        for(int stars=0; stars<nst; stars++) {
+        // stars-2
+        for(int stars=0; stars<nst2; stars++) {
             cout << print;
         }
         cout << endl;
 
-        if(row<=n/2) {
-            nsp--;
-            nst += 2;
-        }
-        else {
-            nsp++;
-            nst -=2;
-        }
+        nsp+=2;
+        nst1--;
+        nst2--;
     }
 
     return 0;
